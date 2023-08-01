0a9799b57bc25941f5c31056446dd8fd41227d96
michaelyuhe <2312744987@qq.com>
2023-08-01 13:03:31 +0800

    feat: Delete project

--- internal/cmd/project/project.go
+++ internal/cmd/project/project.go
@@ -7,6 +7,7 @@ import (
 	"github.com/zeabur/cli/internal/cmdutil"
 
 	projectCreateCmd "github.com/zeabur/cli/internal/cmd/project/create"
+	projectDeleteCmd "github.com/zeabur/cli/internal/cmd/project/delete"
 	projectGetCmd "github.com/zeabur/cli/internal/cmd/project/get"
 	projectListCmd "github.com/zeabur/cli/internal/cmd/project/list"
 )
@@ -23,6 +24,7 @@ func NewCmdProject(f *cmdutil.Factory) *cobra.Command {
 	cmd.AddCommand(projectGetCmd.NewCmdGet(f))
 	cmd.AddCommand(projectListCmd.NewCmdList(f))
 	cmd.AddCommand(projectCreateCmd.NewCmdCreate(f))
+	cmd.AddCommand(projectDeleteCmd.NewCmdDelete(f))
 
 	return cmd
 }
