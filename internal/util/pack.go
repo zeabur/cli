b01693fe0474ccaa8fdaf141592801f168e097c7
michaelyuhe <2312744987@qq.com>
2024-01-23 11:41:29 +0800

    feat: use git archive to create zip file

--- internal/util/pack.go
+++ internal/util/pack.go
@@ -1,125 +1,17 @@
 package util
 
 import (
-	"archive/zip"
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"os"
-	"path/filepath"
-
-	"github.com/denormal/go-gitignore"
+	"os/exec"
 )
 
 func PackZip() ([]byte, string, error) {
-	zipBytes, err := wrapNodeFunction(os.Getenv("PWD"), map[string]string{})
+	format := "--format=zip"
+	output := "--output=zeabur.zip"
 
-	// turn pwd to a valid file name
-	fileName := filepath.Base(os.Getenv("PWD"))
-
+	cmd := exec.Command("git", "archive", format, output, "HEAD")
+	out, err := cmd.CombinedOutput()
 	if err != nil {
-		return nil, "", fmt.Errorf("wrap node function: %w", err)
+		return nil, "", err
 	}
-
-	return zipBytes, fileName, nil
-}
-
-func wrapNodeFunction(baseFolder string, envVars map[string]string) ([]byte, error) {
-	buf := new(bytes.Buffer)
-	w := zip.NewWriter(buf)
-
-	ignore, err := gitignore.NewFromFile(baseFolder + "/.gitignore")
-	if err != nil {
-		return nil, fmt.Errorf("getting gitignore patterns: %w", err)
-	}
-
-	err = filepath.Walk(baseFolder, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return fmt.Errorf("walking to %s: %w", path, err)
-		}
-
-		if info.IsDir() {
-			return nil
-		}
-
-		// This will ensure only the content inside baseFolder is included at the root of the ZIP.
-		relativePath, err := filepath.Rel(baseFolder, path)
-		if err != nil {
-			return fmt.Errorf("getting relative path: %w", err)
-		}
-
-		match := ignore.Match(relativePath)
-		if match != nil {
-			return nil
-		}
-
-		lstat, err := os.Lstat(path)
-		if err != nil {
-			return fmt.Errorf("lstat: %w", err)
-		}
-
-		if lstat.Mode()&os.ModeSymlink == os.ModeSymlink {
-
-			zipFile, err := w.Create(relativePath + ".link")
-			if err != nil {
-				return fmt.Errorf("creating zip file: %w", err)
-			}
-
-			target, err := os.Readlink(path)
-			if err != nil {
-				return fmt.Errorf("read symlink: %w", err)
-			}
-
-			_, err = zipFile.Write([]byte(target))
-			if err != nil {
-				return fmt.Errorf("writing zip file: %w", err)
-			}
-
-		} else {
-
-			zipFile, err := w.Create(relativePath)
-			if err != nil {
-				return fmt.Errorf("creating zip file: %w", err)
-			}
-
-			fileContent, err := os.ReadFile(path)
-			if err != nil {
-				return fmt.Errorf("reading file: %w", err)
-			}
-
-			_, err = zipFile.Write(fileContent)
-			if err != nil {
-				return fmt.Errorf("writing zip file: %w", err)
-			}
-
-		}
-
-		zipFile, err := w.Create(".zeabur-env.json")
-		if err != nil {
-			return fmt.Errorf("creating zip file: %w", err)
-		}
-
-		envJsonStr, err := json.Marshal(envVars)
-		if err != nil {
-			return fmt.Errorf("marshaling env vars: %w", err)
-		}
-
-		_, err = zipFile.Write(envJsonStr)
-		if err != nil {
-			return fmt.Errorf("writing zip file: %w", err)
-		}
-
-		return nil
-	})
-
-	if err != nil {
-		return nil, fmt.Errorf("walking function directory: %w", err)
-	}
-
-	err = w.Close()
-	if err != nil {
-		return nil, fmt.Errorf("closing zip writer: %w", err)
-	}
-
-	return buf.Bytes(), nil
+	return out, output, nil
 }
