4a0f22fff2c3b2dd1e89a61384843bf29a9c2abb
hackerchai <i@hackerchai.com>
2024-01-24 11:10:46 +0800

    feat(models/domain): Modify domain model
    Signed-off-by: hackerchai <i@hackerchai.com>

--- pkg/model/domain.go
+++ pkg/model/domain.go
@@ -3,11 +3,14 @@ package model
 import "time"
 
 type Domain struct {
-	CreatedAt   time.Time `json:"createdAt" graphql:"createdAt"`
-	ID          string    `json:"id" graphql:"_id"`
-	Domain      string    `json:"domain" graphql:"domain"`
-	ProjectID   string    `json:"projectID" graphql:"projectID"`
-	RedirectTo  string    `json:"redirectTo" graphql:"redirectTo"`
-	Status      string    `json:"status" graphql:"status"`
-	IsGenerated bool      `json:"isGenerated" graphql:"isGenerated"`
+	ID            string    `json:"id" graphql:"_id"`
+	Domain        string    `json:"domain" graphql:"domain"`
+	ServiceID     string    `json:"serviceID" graphql:"serviceID"`
+	EnvironmentID string    `json:"environmentID" graphql:"environmentID"`
+	ProjectID     string    `json:"projectID" graphql:"projectID"`
+	PortName      string    `json:"portName" graphql:"portName"`
+	RedirectTo    string    `json:"redirectTo" graphql:"redirectTo"`
+	Status        string    `json:"status" graphql:"status"`
+	IsGenerated   bool      `json:"isGenerated" graphql:"isGenerated"`
+	CreatedAt     time.Time `json:"createdAt" graphql:"createdAt"`
 }
