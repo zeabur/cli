148e9615d28707995713eee790d52315b06fef6a
incubator4 <aries0robin@gmail.com>
2024-06-05 19:18:07 +0800

    feat: add Client function to create custom image service

--- pkg/api/service.go
+++ pkg/api/service.go
@@ -292,6 +292,23 @@ func (c *client) CreatePrebuiltService(ctx context.Context, projectID string, ma
 	return &mutation.CreatePrebuiltService, nil
 }
 
+func (c *client) CreateCustomService(ctx context.Context, projectID string, schema model.ServiceSpecSchemaInput) (*model.Service, error) {
+	var mutation struct {
+		CreateCustomService model.Service `graphql:"createPrebuiltService(projectID: $projectID, schema: $schema)"`
+	}
+
+	err := c.Mutate(ctx, &mutation, V{
+		"projectID": ObjectID(projectID),
+		"schema":    schema,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &mutation.CreateCustomService, nil
+}
+
 func (c *client) SearchGitRepositories(ctx context.Context, keyword *string) ([]model.GitRepo, error) {
 	var query struct {
 		SearchGitRepositories []model.GitRepo `graphql:"searchGitRepositories(Limit: 5, provider: GITHUB, keyword: $keyword)"`
