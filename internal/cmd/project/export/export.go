0d9b9f636507d3962636472ad642f5de61a57b32
Yi-Jyun Pan <pan93412@gmail.com>
2024-09-06 17:12:40 +0800

    feat(project): Implement project export CLI

--- internal/cmd/project/export/export.go
+++ internal/cmd/project/export/export.go
@@ -2,13 +2,20 @@ package export
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/spf13/cobra"
 
 	"github.com/zeabur/cli/internal/cmdutil"
+	"github.com/zeabur/cli/internal/util"
 )
 
-type Options struct{}
+type Options struct {
+	ProjectID   string
+	ProjectName string
+
+	EnvironmentID string
+}
 
 func NewCmdExport(f *cmdutil.Factory) *cobra.Command {
 	opts := Options{}
@@ -21,17 +28,49 @@ func NewCmdExport(f *cmdutil.Factory) *cobra.Command {
 		},
 	}
 
+	util.AddProjectParam(cmd, &opts.ProjectID, &opts.ProjectName)
+	cmd.Flags().StringVar(&opts.EnvironmentID, "environment", "", "Environment ID to export. If not specified, we export the default environment.")
+
 	return cmd
 }
 
-// runExport will list all projects page by page
+// runExport will export the project to a template resource YAML.
 func runExport(f *cmdutil.Factory, opts Options) error {
-	projects, err := f.ApiClient.ListAllProjects(context.Background())
-	if err != nil {
-		return err
+	if opts.ProjectID == "" && opts.ProjectName == "" {
+		return fmt.Errorf("please specify project by --name or --id")
 	}
 
-	f.Printer.Table(projects.Header(), projects.Rows())
+	if opts.ProjectID == "" && opts.ProjectName != "" {
+		project, err := util.GetProjectByName(f.Config, f.ApiClient, opts.ProjectName)
+		if err != nil {
+			return fmt.Errorf("get project %s failed: %w", opts.ProjectName, err)
+		}
+		opts.ProjectID = project.ID
+	}
+
+	if opts.EnvironmentID == "" {
+		environments, err := f.ApiClient.ListEnvironments(context.Background(), opts.ProjectID)
+		if err != nil {
+			return fmt.Errorf("list environments for project<%s> failed: %w", opts.ProjectID, err)
+		}
+
+		if len(environments) == 0 {
+			return fmt.Errorf("no environment found in project %s", opts.ProjectID)
+		}
+
+		opts.EnvironmentID = environments[0].ID
+	}
+
+	exportedTemplate, err := f.ApiClient.ExportProject(context.Background(), opts.ProjectID, opts.EnvironmentID)
+	if err != nil {
+		return fmt.Errorf("export environment<%s> of project<%s> failed: %w", opts.EnvironmentID, opts.ProjectID, err)
+	}
+
+	for _, warning := range exportedTemplate.Warnings {
+		f.Log.Warn(warning)
+	}
+
+	fmt.Println(exportedTemplate.ResourceYAML)
 
 	return nil
 }
